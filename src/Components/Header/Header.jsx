d519179b7915e5a6142550f17e6f5548bea67989
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-26 13:05:49 +0300

    Yangidan ochdim

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='container header-container'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
