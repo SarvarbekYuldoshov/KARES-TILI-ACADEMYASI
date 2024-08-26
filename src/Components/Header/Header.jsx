b47a6f94a5cbea5aa49249031ca5a3584a3df5c3
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-26 14:41:36 +0300

    salom

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Header.css"
+import Rasm_1 from "../images/rasm_1.png"
 import { useTranslation } from 'react-i18next';
 const Header = () => {
   const {t,i18n} = useTranslation();
@@ -10,24 +11,28 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='container header-container'>
-        <ul className='header-list'>
-          <img className='header-img' src="" alt="" />
+        <ul className='header-list-a'>
+          <img className='header-img' src={Rasm_1} alt="" />
           <p className='header-text'>{t("header.header_text")}</p>
         </ul>
         <ul className='header-list'>
-          <li className='header-item'><a className='header-link' href=""></a></li>
-          <li className='header-item'><a className='header-link' href=""></a></li>
-          <li className='header-item'><a className='header-link' href=""></a></li>
-          <li className='header-item'><a className='header-link' href=""></a></li>
+            <li className='header-item'><a className='header-link' href="">{t("header.header_link_1")}</a></li>
+            <li className='header-item'><a className='header-link' href="">{t("header.header_link_2")}</a></li>
+            <li className='header-item'><a className='header-link' href="">{t("header.header_link_3")}</a></li>
+            <li className='header-item'><a className='header-link' href="">{t("header.header_link_4")}</a></li>
         </ul>
         <ul className='header-list'></ul>
-        <li className='header-item'>
-                    <select name="" id="" onChange={handleLaguage} value={i18n.language}>
-                      <option value="uz">Uzbek</option>
-                      <option value="ru">Russian</option>
-                      <option value="en">English</option>
-                    </select>
-                </li>
+          <li className='header-item'>
+            <select name="" id="" onChange={handleLaguage} value={i18n.language}>
+              <option value="uz">Uzbek</option>
+              <option value="ru">Russian</option>
+              <option value="en">English</option>
+            </select>
+          </li>
+          <li className='header-item'>
+            <h1 className='header-title'>+998 (33) 306 0098</h1>
+            <p className='header-desc'>{t("header.header_title")}</p>
+          </li>  
       </div>
     </div>
   )
