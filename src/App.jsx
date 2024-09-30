e56a6b9a03ef2b7fb1cf7071729a3005edc4feba
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-30 08:44:57 +0300

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
           <Number/>
           <Close/>
           <Main/>
-          <Faq/>
+          {/* <Faq/> */}
           {/* <Section/> */}
           {/* <Footer/> */}
       </div>
